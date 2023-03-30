5666d76e7b2f8bccd6af297fe94ba34505b53edd
JdJaku <jaimin.desai@onboardops.com>
2023-03-30 02:33:57 -0300

    Added drawer to edit task and updated edit task activity.

--- app/src/main/java/com/example/progresspal/editTask.kt
+++ app/src/main/java/com/example/progresspal/editTask.kt
@@ -5,31 +5,59 @@ import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.text.format.DateFormat
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.example.progresspal.databinding.ActivityAddTaskBinding
+import com.example.progresspal.databinding.ActivityEditTaskBinding
 import com.example.progresspal.persistence.TaskPersistence
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.Timestamp
 import java.sql.Time
 import java.util.*
 
 
-class editTask : AppCompatActivity() {
+class editTask : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var binding: ActivityEditTaskBinding
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationView: NavigationView
+    private lateinit var toolbar: androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_task)
+        binding = ActivityEditTaskBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val originalTask: Task
         var task: Task
         task = getIntent().getParcelableExtra("task")!!
         originalTask = getIntent().getParcelableExtra("task")!!
         var position = intent.getIntExtra("pos", 0)
 
-        val taskName = findViewById<TextView>(R.id.editTaskName)
-        taskName.text = task.title
+        drawerLayout = binding.drawerLayout
+        navigationView = binding.navView
+        toolbar = binding.menuBtn
+
+        setSupportActionBar(toolbar);
+        supportActionBar!!.setIcon(R.drawable.menu)
+
+        navigationView.bringToFront()
+        val toggle: ActionBarDrawerToggle =
+            ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener(this)
 
 
-        val datePickerOpen = findViewById<Button>(R.id.editDatePickerOpen)
-        val date = Date(task.dueDate.seconds*1000)
+        val taskName = binding.editTaskName
+        taskName.setText(task.getTitle())
+
+
+        val datePickerOpen = binding.editDatePickerOpen
+        val date = Date(task.dueDate.seconds * 1000)
 
 
         val stringDay = DateFormat.format("dd", date) as String
@@ -44,14 +72,15 @@ class editTask : AppCompatActivity() {
         datePickerOpen.text = "$day-$month-$year"
 
 
-        datePickerOpen.setOnClickListener(){
-            val datePickerDialog = DatePickerDialog(this, android.R.style.Widget_CalendarView,
+        datePickerOpen.setOnClickListener() {
+            val datePickerDialog = DatePickerDialog(
+                this, android.R.style.Widget_CalendarView,
                 { datePicker, year, month, day -> //Showing the picked value in the textView
                     val tempMonth = month + 1
                     pickedDate = "$day-$tempMonth-$year"
                     datePickerOpen.text = pickedDate
                     pickedDate = "$year-$month-$day"
-                }, year, month-1, day
+                }, year, month - 1, day
             )
             datePickerDialog.datePicker.minDate = System.currentTimeMillis()
             datePickerDialog.show()
@@ -60,14 +89,15 @@ class editTask : AppCompatActivity() {
         // priorities code
         var priority = "Low"
         val priorities = arrayOf("High", "Medium", "Low")
-        val spinner = findViewById<Spinner>(R.id.editPrioritySpinner)
-        val priorityAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, priorities)
+        val spinner = binding.editPrioritySpinner
+        val priorityAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, priorities)
 
         val spinnerPosition = priorityAdapter.getPosition(task.priority)
         spinner.adapter = priorityAdapter
         spinner.setSelection(spinnerPosition, true)
 
-        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 priority = priorities[p2]
             }
@@ -79,11 +109,11 @@ class editTask : AppCompatActivity() {
 
 
         // time picker code
-        val timePickerOpen = findViewById<Button>(R.id.editTimePickerOpen)
+        val timePickerOpen = binding.editTimePickerOpen
 
-        var numberTime : Int
+        var numberTime: Int
 
-        val time = Date(task.reminder.seconds*1000)
+        val time = Date(task.reminder.seconds * 1000)
         val stringHour = DateFormat.format("HH", time) as String
         val stringMinute = DateFormat.format("mm", time) as String
         val amORpm = DateFormat.format("a", time) as String
@@ -91,8 +121,8 @@ class editTask : AppCompatActivity() {
         var hour = stringHour.toInt()
         var minute = stringMinute.toInt()
 
-        if(amORpm == "PM"){
-            if(hour == 24){
+        if (amORpm == "PM") {
+            if (hour == 24) {
                 hour = 0
             }
         }
@@ -100,12 +130,13 @@ class editTask : AppCompatActivity() {
         var pickedTime = "$hour:$minute"
         timePickerOpen.text = pickedTime
 
-        timePickerOpen.setOnClickListener(){
-            val timePickerDialog = TimePickerDialog(this,{ timePicker: TimePicker, i: Int, i1: Int ->
-                pickedTime = "$i:$i1"
-                numberTime = i + (i1/100)
-                timePickerOpen.text = pickedTime
-            }, hour, minute, true)
+        timePickerOpen.setOnClickListener() {
+            val timePickerDialog =
+                TimePickerDialog(this, { timePicker: TimePicker, i: Int, i1: Int ->
+                    pickedTime = "$i:$i1"
+                    numberTime = i + (i1 / 100)
+                    timePickerOpen.text = pickedTime
+                }, hour, minute, true)
 
             timePickerDialog.show()
         }
@@ -113,17 +144,19 @@ class editTask : AppCompatActivity() {
         // repeat code
         var repeatWhen = "None"
         val repeat = arrayOf("None", "Daily", "Weekly", "Monthly")
-        val repeatSpinner = findViewById<Spinner>(R.id.editRepeatSpinner)
-        val repeatAdapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, repeat)
+        val repeatSpinner = binding.editRepeatSpinner
+        val repeatAdapter =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, repeat)
 
         val repeatSpinnerPosition = repeatAdapter.getPosition(task.repeat)
         repeatSpinner.adapter = repeatAdapter
         repeatSpinner.setSelection(repeatSpinnerPosition, true)
 
-        repeatSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        repeatSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
                 repeatWhen = repeat[p2]
             }
+
             override fun onNothingSelected(p0: AdapterView<*>?) {
             }
         }
@@ -131,32 +164,58 @@ class editTask : AppCompatActivity() {
 
         val intent = Intent(this, MainActivity::class.java)
 
-        findViewById<Button>(R.id.editTaskDeleteBtn).setOnClickListener{
+        binding.editTaskDeleteBtn.setOnClickListener {
             //startActivity(intent)
         }
 
 
-        findViewById<Button>(R.id.editTaskSaveChangesBtn).setOnClickListener{
+        binding.editTaskSaveChangesBtn.setOnClickListener {
             println(priority)
             println(repeatWhen)
-            var date : Timestamp
-            var time : Timestamp
-            if(pickedDate == "$day-$month-$year"){
+            var date: Timestamp
+            var time: Timestamp
+            if (pickedDate == "$day-$month-$year") {
                 date = task.dueDate
-            }
-            else{
+            } else {
                 date = Timestamp(java.sql.Date.valueOf(pickedDate))
             }
-            if(pickedTime == "$hour:$minute"){
+            if (pickedTime == "$hour:$minute") {
                 time = task.reminder
+            } else {
+                time = Timestamp(Time.valueOf(pickedTime + ":00"))
             }
-            else{
-                time = Timestamp(Time.valueOf(pickedTime+":00"))
-            }
-            val first = Task(0,"${taskName.text} \uD83D\uDE80", priority, date,time, repeatWhen,false );
+            val first =
+                Task(0, "${taskName.text} \uD83D\uDE80", priority, date, time, repeatWhen, false);
             TaskPersistence.edit(first, position, originalTask)
             startActivity(intent)
         }
 
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.archived -> {
+                val intent = Intent(this, Archived::class.java)
+                startActivity(intent)
+            }
+            R.id.stats -> {
+                val intent = Intent(this, Stats::class.java)
+                startActivity(intent)
+            }
+        }
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
